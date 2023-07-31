02ec631f62ff9d17b34d42eaf330adf8a4c6d7d7
Lefan Hu <lefanhu1@gmail.com>
2023-07-30 22:54:52 -0400

    upload working

--- functions/split_video/app.py
+++ functions/split_video/app.py
@@ -1,17 +1,46 @@
 import boto3
 import os
 from moviepy.editor import VideoFileClip
+import logging
+from botocore.exceptions import ClientError
 
 client = boto3.client("lambda")
 
 def extractAudio(video_file, output_ext="mp3"):
-    """Converts video to audio using MoviePy library"""
+    """Converts video to audio using MoviePy library
+
+    :return: filepath of where audio file was written
+    """
 
     filename, ext = os.path.splitext(video_file)
     clip = VideoFileClip(video_file)
     print("writing file: " + filename)
     clip.audio.write_audiofile(f"{filename}.{output_ext}")
 
+    return f"{filename}.{output_ext}"
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 def lambda_handler(event, context):
     event = event['Input']
@@ -22,11 +51,18 @@ def lambda_handler(event, context):
     filename = event['key']
     directory = "/tmp/{}".format(filename)
 
-    print("Downloading file: " + filename)
+    logging.info("Downloading file: " + filename)
     s3.Bucket(bucket).download_file(filename, directory)
 
+
     # extract audio
-    extractAudio(directory)
+    logging.info("Extracting audio")
+    filename = extractAudio(directory)
+
+
+    # upload
+    logging.info("Uploading audiofile")
+    upload_file(filename, "interqu-audio")
         
     os.popen("rm -rf /tmp")
     
