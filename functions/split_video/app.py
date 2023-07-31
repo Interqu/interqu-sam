a7e58b621e760d9b1ec5df43e300d3e3ab5bb564
Lefan Hu <lefanhu1@gmail.com>
2023-07-30 22:39:49 -0400

    audio extraction working

--- functions/split_video/app.py
+++ functions/split_video/app.py
@@ -1,8 +1,17 @@
-import json
 import boto3
-import requests
+import os
+from moviepy.editor import VideoFileClip
 
 client = boto3.client("lambda")
+
+def extractAudio(video_file, output_ext="mp3"):
+    """Converts video to audio using MoviePy library"""
+
+    filename, ext = os.path.splitext(video_file)
+    clip = VideoFileClip(video_file)
+    print("writing file: " + filename)
+    clip.audio.write_audiofile(f"{filename}.{output_ext}")
+
 
 def lambda_handler(event, context):
     event = event['Input']
@@ -12,12 +21,13 @@ def lambda_handler(event, context):
     bucket = event['bucket'].split(':')[-1]
     filename = event['key']
     directory = "/tmp/{}".format(filename)
-    
+
+    print("Downloading file: " + filename)
     s3.Bucket(bucket).download_file(filename, directory)
-    
-    with open(directory, "r") as jsonfile:
-        final_json = json.load(jsonfile)
-    
+
+    # extract audio
+    extractAudio(directory)
+        
     os.popen("rm -rf /tmp")
     
     return final_json
